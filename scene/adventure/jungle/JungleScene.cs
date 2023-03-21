63dbe3e1fc849c661b8440b748ab4295783a98bf
blessingIX <1350437095@qq.com>
2023-03-21 23:17:18 +0800

    feat: 丛林、沙漠、深海主题选择关卡

---
+++ scene/adventure/jungle/JungleScene.cs
@@ -0,0 +1,7 @@
+namespace CarrotFantasy.scene.adventure.jungle
+{
+    public partial class JungleScene : SelectLevelScene
+    {
+        // TODO JungleScene.tscn挂载此类报错
+    }
+}
--- scene/adventure/skyline/SkylineScene.cs
+++ scene/adventure/skyline/SkylineScene.cs
@@ -2,6 +2,6 @@ namespace CarrotFantasy.scene.adventure.skyline
 {
     public partial class SkylineScene : SelectLevelScene
     {
-
+        // TODO SkylineScene.tscn挂载此类报错
     }
 }
